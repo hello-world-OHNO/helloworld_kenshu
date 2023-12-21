008412d520463f074a3a412d45588e752c7f06dd
大野　洋平 <daruma0248@gmail.com>
2023-12-21 19:32:28 +0900

    課題３修了

--- js/index.js
+++ js/index.js
@@ -3,3 +3,36 @@ console.log(text)
 
 var text2 = document.querySelector("#text")
 console.log(text2)
+
+// 空pタグの生成、div内に追加
+var p1 = document.getElementById("div1")
+
+var newP = document.createElement("p")
+
+p1.appendChild(newP)
+//
+
+// clickされたらconsoleに"click"が表示
+var btn = document.querySelector("#button")
+
+btn.addEventListener("click", () => {
+  console.log("click")
+})
+//
+
+// scrollされたらconsoleに"scroll"と表示
+var scr = document.querySelector("#scroll")
+
+window.addEventListener("scroll", function () {
+  console.log("scroll")
+})
+//
+
+// buttonクリックで、子要素が追加、テキスト表示
+var btn2 = document.querySelector("#button2")
+var result = document.querySelector("#result")
+
+btn2.addEventListener("click", () => {
+  result.innerHTML = "よく押せました！えらい！"
+})
+// 
